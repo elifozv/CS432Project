a5b7fba7dbb86ccee9c6de883d1fc120eb7e816a
dodo146 <digdeci@gmail.com>
2023-05-02 21:03:29 +0300

    Fixed the verify signature issue and added extra logs statements Also fixed the split " " issue

--- Client/Client/Form1.cs
+++ Client/Client/Form1.cs
@@ -19,9 +19,10 @@ namespace Client
         bool terminating = false;
         bool connected = false;
         Socket clientSocket;
+        string channel = "null";
 
         byte[] publicKey = File.ReadAllBytes("server_enc_dec_pub.txt");
-        byte[] signature = File.ReadAllBytes("server_sign_verify_pub.txt");
+        byte[] server_signature = File.ReadAllBytes("server_sign_verify_pub.txt");
 
         public Form1()
         {
@@ -71,27 +72,26 @@ namespace Client
         {
             string username = userText.Text;
             string password = passText.Text;
-            string channel = "null";
             if (radioButton1.Checked) channel = "IF100";
             else if (radioButton2.Checked) channel = "MATH101";
             else if (radioButton3.Checked) channel = "SPS101";
 
             password = Encoding.Default.GetString(hashWithSHA512(password));
             string pubString = Encoding.Default.GetString(publicKey);
-            string credentials = "username:"+username + " password:" + password + " channel:" + channel;
+            string credentials = "username:"+username + "password:" + password + "channel:" + channel;
             byte[] encryptedMsg = encryptWithRSA(credentials, 3072, pubString);
 
             try
             {
                 clientSocket.Send(encryptedMsg);
-
+                logs.AppendText("Submit button clicked. Trying to enroll...\n");
                 Thread receiveThread = new Thread(ReceiveMessage);
                 receiveThread.Start();
                 
             }
             catch
             {
-                logs.AppendText("The encrypted message couldn't be sent");
+                logs.AppendText("The encrypted message couldn't be sent\n");
             }
 
         }
@@ -102,16 +102,19 @@ namespace Client
             {
                 try
                 {
-                    Byte[] buffer = new Byte[384];
+                    Byte[] buffer = new Byte[64];
+                    Byte[] signature_byte = new byte[384]; 
                     clientSocket.Receive(buffer);
+                    clientSocket.Receive(signature_byte);
+                    string server_pub = Encoding.Default.GetString(server_signature);
 
                     string message = Encoding.Default.GetString(buffer);
-                    //message = message.Substring(0, message.IndexOf("\0"));
+                    message = message.Substring(0, message.IndexOf("\0"));
     
 
                     string pubString = Encoding.Default.GetString(publicKey);
 
-                    if (verifyWithRSA(message, 3072, pubString, signature))
+                    if (verifyWithRSA(message, 3072,server_pub ,signature_byte))
                     {
                         if (message == "success")
                         {
